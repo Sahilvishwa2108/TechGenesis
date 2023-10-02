83542699af88202a5bf8b359d710b5085b99a992
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2023-10-03 01:20:50 +0530

    callback hell solved using asynch and await

--- signup.js
+++ signup.js
@@ -1,39 +1,23 @@
-var typed = new Typed("#element1", {
-    strings: ["<h2>Sign Up</h2>"],
-    typeSpeed: 10,
-    showCursor: false,
-    onComplete: function () {
-        var typed = new Typed("#element2", {
-            strings: ["Email:"],
-            typeSpeed: 10,
+function typeText(elementId, text, typeSpeed = 10) {
+    return new Promise((resolve) => {
+        var typed = new Typed(elementId, {
+            strings: [text],
+            typeSpeed,
             showCursor: false,
-            onComplete: function () {
-                var typed = new Typed("#element3", {
-                    strings: ["Password:"],
-                    typeSpeed: 10,
-                    showCursor: false,
-                    onComplete: function () {
-                        var typed = new Typed("#element4", {
-                            strings: ["Confirm Password?"],
-                            typeSpeed: 10,
-                            showCursor: false,
-                            onComplete: function () {
-                                var typed = new Typed("#element5", {
-                                    strings: ["Already have an account?"],
-                                    typeSpeed: 10,
-                                    showCursor: false,
-                                    onComplete: function () {
-                                        var typed = new Typed("#element6", {
-                                            strings: ["Login"],
-                                            typeSpeed: 10,
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
+            onComplete: () => {
+                resolve();
+            },
         });
-    }
-});
+    });
+}
+
+async function startTyping() {
+    await typeText("#element1", "<h2>Sign Up</h2>");
+    await typeText("#element2", "Email:");
+    await typeText("#element3", "Password:");
+    await typeText("#element4", "Confirm Password?");
+    await typeText("#element5", "Already have an account?");
+    await typeText("#element6", "Login", 10);
+}
+
+startTyping();
